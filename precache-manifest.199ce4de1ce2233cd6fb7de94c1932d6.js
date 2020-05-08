ab5ad56215d68699173fed3367bf49f961ef6c87
mugnimaestra <mugnimaestra3@gmail.com>
2020-05-09 06:45:42 +0700

    Updates

---
+++ precache-manifest.199ce4de1ce2233cd6fb7de94c1932d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4a46ec56c577c24b584d78b42911285",
+    "url": "/pokeapi-react/index.html"
+  },
+  {
+    "revision": "48ae7559cf9570293ead",
+    "url": "/pokeapi-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9f1df2fe44d706390aac",
+    "url": "/pokeapi-react/static/js/2.43539815.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/pokeapi-react/static/js/2.43539815.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48ae7559cf9570293ead",
+    "url": "/pokeapi-react/static/js/main.23ff1938.chunk.js"
+  },
+  {
+    "revision": "171b6fff1663b1cb24f2",
+    "url": "/pokeapi-react/static/js/runtime-main.daabf1a0.js"
+  }
+]);
