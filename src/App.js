4f9127b0d115541770350355c1f39311c4fa2dba
mugnimaestra <mugnimaestra3@gmail.com>
2020-05-09 06:05:08 +0700

    added routing for app with react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './App.css';
+import PokemonGrid from './components/PokemonGrid';
+import NotFound from './pages/NotFound';
+import PokemonDetails from './pages/PokemonDetails';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact render={props => <PokemonGrid {...props} />} />
+          <Route path="/pokemon/:_name" component={PokemonDetails} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
